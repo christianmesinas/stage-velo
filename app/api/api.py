c6080964497acec785e4732018823db2d2037af4
christian <guzmanchristian@hotmail.com>
2025-04-24 11:32:30 +0200

    testing api adress

--- app/api/api.py
+++ app/api/api.py
@@ -8,6 +8,36 @@ def get_info():
     response = requests.get(BASE_URL, headers=HEADERS, params=params)
     if response.status_code == 200:
         data = response.json()
-        return data
+        stations = data['network']['stations']
+        result = [
+            {
+                'id': station['id'],
+                'name': station['name'],
+                'location': {
+                    'latitude': station['latitude'],
+                    'longitude': station['longitude']
+                },
+                'free_bikes': station['free_bikes'],
+                'empty_slots': station['empty_slots'],
+                'extra': {
+                    'adress': station['extra']['address'],
+                }
 
-print(get_info())
+            }
+            for station in stations
+        ]
+        return result
+    else:
+        return None
+
+
+stations_info = get_info()
+if stations_info:
+    for station in stations_info:
+        print(f"ID: {station['id']}")
+        print(f"Naam: {station['name']}")
+        print(f"Locatie: {station['location']['latitude']}, {station['location']['longitude']}")
+        print(f"heeft zoveel vrije fietsen: {station['free_bikes']}")
+        print(f"heeft zoveel vrije plaatsen: {station['empty_slots']}")
+        print(f"bij adress: {station['extra']['adress']}")
+        print("-" * 50)
