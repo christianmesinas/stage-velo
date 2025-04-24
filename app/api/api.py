e7f4e150e43fec3daf1f7dc2f9831be4da4bb1c9
MathijsAdriaensen1 <mathijs.adriaensen1@gmail.com>
2025-04-24 12:09:21 +0200

    Beschrijf je wijzigingen

--- app/api/api.py
+++ app/api/api.py
@@ -8,43 +8,6 @@ def get_info():
     response = requests.get(BASE_URL, headers=HEADERS, params=params)
     if response.status_code == 200:
         data = response.json()
-        stations = data['network']['stations']
-        result = [
-            {
-                'id': station['id'],
-                'name': station['name'],
-                'location': {
-                    'latitude': station['latitude'],
-                    'longitude': station['longitude']
-                },
-                'free_bikes': station['free_bikes'],
-                'empty_slots': station['empty_slots'],
-                'extra': {
-                    'adress': station['extra']['address'],
-                }
+        return data
 
-            }
-            for station in stations
-        ]
-        return result
-    else:
-        return None
-
-
-stations_info = get_info()
-
-def print_alle_date():
-    if stations_info:
-        for station in stations_info:
-            print(f"ID: {station['id']}")
-            print(f"Naam: {station['name']}")
-            print(f"Locatie: {station['location']['latitude']}, {station['location']['longitude']}")
-            print(f"heeft zoveel vrije fietsen: {station['free_bikes']}")
-            print(f"heeft zoveel vrije plaatsen: {station['empty_slots']}")
-            print(f"bij adress: {station['extra']['adress']}")
-            print("-" * 50)
-
-def zoek_lege_slots():
-    for station in stations_info:
-        if station['empty_slots'] > 0:
-            print(f"er zijn {station['empty_slots']} lege slots bij de station op {station['extra']['adress']}")
+print(get_info())
